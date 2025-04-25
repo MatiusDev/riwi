aa121af80979e2259bef2633ca4ae817598f2412
MatiusDev <thematius.monsalve@gmail.com>
2025-04-24 21:43:46 -0500

    New: Agregando sistema de productos

--- w1_module_python/w1_moodle/projects/training/main.py
+++ w1_module_python/w1_moodle/projects/training/main.py
@@ -1,29 +1,4 @@
-# Desarrollar un programa que demuestre tu capacidad para aplicar conceptos básicos de entrada de datos, tipos de datos, y operadores en Python.
-# Paso 1 	Análisis y planificación del problema: Lee y analiza el enunciado para comprender los requisitos. Lista las tareas que el programa debe realizar para mantener un flujo ordenado y no omitir ningún elemento.
-# Paso 2 	Entrada y validación de datos: Implementa la captura de datos desde la consola, solicitando toda la información necesaria. Asegúrate de que los datos ingresados sean del tipo y formato correctos para evitar errores en el procesamiento.
-# Paso 3 	Desarrollo de la lógica del programa: Divide el problema en subtareas más pequeñas para facilitar el desarrollo. Aplica operadores aritméticos, relacionales y lógicos para procesar los datos ingresados y resolver el problema.
-# Paso 4 	Generación y formato de resultados: Muestra los resultados en la consola de forma clara y legible, considerando varios casos para garantizar su comprensión.
-# Paso 5 	Pruebas y revisión: Realiza pruebas con entradas variadas, incluyendo casos límite, para asegurar que el programa funcione correctamente. Revisa el código en busca de errores y asegúrate de que sea estructurado y coherente.
-# Paso 6 	Entrega: Comprime tu código en un archivo .zip o proporciona un enlace a un repositorio público de GitHub y súbelo a Moodle antes de la fecha límite.
-
-def is_a_number(str):
-    if (len(str) == 0):
-        return False
-    
-    try:
-        int(str)
-        return True
-    except ValueError:
-        return False
-    
-def valid_option(opt):
-    if (not is_a_number(opt)):
-        return False
-    
-    num_opt = int(opt)
-    if (num_opt < -1 or num_opt > 2):
-        return False
-    return True
+import random
 
 USERS = [
     {
@@ -54,12 +29,105 @@ USERS = [
 
 opts = [
     "Mostrar productos",
+    "Agregar producto",
+    "Editar producto",
+    "Eliminar producto"
+]
+
+opts_admin = opts + [
     "Mostrar usuarios"
 ]
 
+products = [
+    {
+        "id": 12345,
+        "name": "Manzana",
+        "price": 5000,
+        "amount": 2,
+        "discount": 0
+    },
+    {
+        "id": 234123412,
+        "name": "Plátano",
+        "price": 3000,
+        "amount": 1,
+        "discount": 0
+    },
+    {
+        "id": 3213211,
+        "name": "Naranja",
+        "price": 2500,
+        "amount": 4,
+        "discount": 0
+    }
+]
+
+def is_a_number(str):
+    if (len(str) == 0):
+        return False
+    
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
+    
+def valid_option(opt):
+    if (not is_a_number(opt)):
+        return False
+    
+    num_opt = int(opt)
+    if (num_opt < -1 or num_opt > 2):
+        return False
+    return True
+
+def show_action(opt, user, id_product):
+    if (type(opt) != int):
+        print("\nHa ocurrido un error, por favor consulta un administrador")
+        return
+    ADMIN = opts_admin.index("Mostrar usuarios")+1
+
+    match opt:
+        case 1:
+            print("*** CARRITO ***")
+            print(f"\n{'Nombre producto'.ljust(20)}| {'Precio'.ljust(10)}| {'Cantidad'.ljust(15)}| {'Descuento'}")
+            print("-------------------------------------------------------------")
+            for product in products:
+                print(f"{str(product["name"]).ljust(20)}| {str(product["price"]).ljust(10)}| {str(product["amount"]).ljust(15)}| {product["discount"]}" )
+        case 2:# Agregar producto
+            print("*** NUEVO PRODUCTO ***")
+            name = input("Ingrese el nombre: ")
+            price = float(input("Ingrese el precio: "))
+            amount = int(input("Ingrese la cantidad: "))
+            discount = float(input("Ingrese el descuento (%): "))
+            id = f"2025{random.randrange(0, 2025) * random.randint(1, 13)}"
+            product = {
+                "id": id,
+                "name": name,
+                "price": price,
+                "amount": amount,
+                "discount": discount
+            }
+            products.append(product)
+        case 3:# Editar producto
+            pass
+        case 4:# Eliminar producto
+            pass
+        case _ if opt == ADMIN:
+            if not user['is_admin']:
+                print("Ingresaste una opción invalida")
+                return
+            
+            print(f"\n{'Nombre completo'.ljust(20)}| {'Nombre de usuario'.ljust(25)}| {'Edad'.ljust(5)}| {'Role'}")
+            print("----------------------------------------------------------------------")
+            for user in USERS:
+                print(f"{str(user["fullname"]).ljust(20)}| {str(user["username"]).ljust(25)}| {str(user["age"]).ljust(5)}| {"Administrador" if user["is_admin"] else "Usuario"}" )
+        case _:
+            print("Ingresaste una opción invalida")
+
 def start():
     while(True):
-        print("*** BIENVENIDO AL SISTEMA DE INVENTARIO ***\n")
+        print("*** BIENVENIDO AL CARRITO DE COMPRAS ***\n")
 
         opt = input("1. Ingresar sesión\n" \
         "0. Salir\n" \
@@ -90,16 +158,24 @@ def start():
         while(True):
             print("")
             print(f"Bienvenido de nuevo {user['fullname']}, ¿Qué deseas hacer hoy?")
-            for i in range(len(opts)):
-                print(f"{i+1}. {opts[i]}")
+
+            my_opts = opts_admin if user['is_admin'] else opts
+
+            for i in range(len(my_opts)):
+                print(f"{i+1}. {my_opts[i]}")
             print("0. Salir")
             action = input("Seleccione su opción: ")
+                        
+            if not is_a_number(action):
+                print("Por favor ingrese una opción valida.")
+                continue
+
             action = int(action)
-            
+
             if (action == 0):
                 break
 
-            print(f"La opción que elegiste fue: {opts[action-1]}")            
+            show_action(action, user, 0)          
         print(f"\nGracias por usar nuestro software {user["fullname"]}\n")
     print(f"Recuerda calificar tu experiencia con la aplicación, ten un buen día :)")
 
